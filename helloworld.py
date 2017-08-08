e1f68dbf3de17bdf89fb53ad3e513883efeac4dd
lifan <lifan@gozap.com>
2017-08-08 17:31:42 +0800

    测试提交

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+
+# 输出值
+print("hello-world 你好")
+
+# 接收多个字符串, 用逗号“,”隔开, 就可以连成一串输出, 遇到逗号会变成一个空格
+
+print("hello world", "hello lindira", "hello index")
+
+print(100 + 200)
+
+print("100 + 200 =", 100 + 200)
+
+# 输入值
+
+name = input()
+
+print(name)
+
+
+
