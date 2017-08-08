4e733f744c27471b9e7a706d98e5acef327f9b8b
indira <18804058485@163.com>
2017-08-08 21:32:44 +0800

    测试提交

--- helloworld.py
+++ helloworld.py
@@ -3,27 +3,20 @@
 print("hello-world 你好")
 
 # 接收多个字符串, 用逗号“,”隔开, 就可以连成一串输出, 遇到逗号会变成一个空格
-
 print("hello world", "hello lindira", "hello index")
 
+# 输出时算式
 print(100 + 200)
-
 print("100 + 200 =", 100 + 200)
 
 # 输入值
-
 name = input()
-
 print(name)
 
 # 加入输入时提示信息
-
 name = input('please enter your name: ')
-
 print('hello,', name)
 
 # 特殊字符\隔开
-
 print('I\'m ok.')
-
 print('I\'m learning\nPython.')
