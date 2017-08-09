f5e1308057cbf580a1710d431d52184865c099d0
indira <18804058485@163.com>
2017-08-09 22:44:40 +0800

    ifelse

---
+++ IfElse.py
@@ -0,0 +1,21 @@
+
+age = 20
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+
+age = 3
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+else:
+    print('your age is', age)
+    print('teenager')
+
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
